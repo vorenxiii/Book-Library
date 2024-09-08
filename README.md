96c7992d57bf615f44a69e655239f51c74e24240
catsucci <150689455+catsucci@users.noreply.github.com>
2024-09-08 14:35:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Book's Library
+
+This is a part of the Odin Project Course, in this case, it is a book's library.
+
+## Technologies used:
+* HTML
+* CSS
+* Javascript
+* Git
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+("use strict");
+
+// cspell: words: Catsucci
+
+/**
+ * main.js
+ * Main JavaScript file for the project
+ * Author: Catsucci
+ * Created: 2024-09-08
+ */
+
+console.log("Hello, World!");
